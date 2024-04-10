e5475151e27adc281435f586d27e81e702c6e9aa
priya2614 <165402467+priya2614@users.noreply.github.com>
2024-04-10 04:43:20 -0700

    Add files via upload

---
+++ method.js
@@ -0,0 +1,82 @@
+
+// conststructor methods
+
+let today = new Date();
+console.log(today);
+
+let day = today.getDay();
+console.log( 'day',day);
+
+let year = today.getFullYear();
+console.log( 'year', year);
+
+let  minutes = today.getMinutes();
+console.log( 'minutes',minutes);
+
+let millisecs = today.getMilliseconds();
+console.log( 'millisecs', millisecs);
+
+let month = today. getMonth() + 1 ;
+console.log('month', month);
+
+let hours = today.getHours();
+console.log('hours', hours);
+
+//number method
+  let pi = 3.14;
+  let get =pi.toFixed  console.log( 'pi value',get);
+
+
+//math methods 
+ let num1 = 250;
+  let num2 = 143;
+  let min = Math.min(num1,num2);
+  console.log( 'min value', min);
+
+ let num3 = 1004;
+ let num4 = 5096;
+ let max = Math.max(num3,num4)
+ console.log('max value',max);
+
+
+
+
+let number = 36;
+let square = Math.sqrt(number);
+console.log( 'root num',square);  //square num
+
+let butterNaan =  79.99;
+let round = Math.round(butterNaan);  
+console.log( 'butter Naan',round);  //rounded
+
+
+let noodles = 90.45;
+let floor = Math.floor(noodles);
+console.log('noodles',floor);      //floor
+
+let noodless = 90.44;
+let ceil = Math.ceil(noodless);
+console.log('noodless', ceil);      //ceil
+
+
+let randamNum = Math.random();
+let numbering = (randamNum * 999) + 1;
+console.log('random num',Math.ceil(numbering));  //randam number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
